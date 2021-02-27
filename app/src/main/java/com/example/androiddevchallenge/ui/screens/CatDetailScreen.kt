5338fe1b2e85378e8c156d4d25d54a82b8800dc9
hotatekaoru <kaorupochi@gmail.com>
2021-02-27 16:50:07 +0900

    Divide UI logic from MainActivity

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/CatDetailScreen.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavController
+import com.example.androiddevchallenge.model.Cat
+import com.example.androiddevchallenge.model.catsSeed
+
+@Composable
+fun CatDetailScreen(
+    navController: NavController,
+    catId: Int
+) {
+    catsSeed.find { it.id == catId }?.also { cat ->
+        CatDetailView(cat)
+    } ?: run {
+        navController.navigateUp()
+    }
+}
+
+@Composable
+fun CatDetailView(cat: Cat) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(cat.name)
+                }
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(cat.image), contentDescription = cat.name)
+            Text(text = cat.name)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/CatListScreen.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.navigate
+import com.example.androiddevchallenge.model.Cat
+import com.example.androiddevchallenge.model.catsSeed
+
+@Composable
+fun CatListScreen(
+    navController: NavController,
+    cats: List<Cat> = catsSeed
+) {
+    LazyColumn(Modifier.fillMaxSize()) {
+        items(cats) { cat ->
+            CatCard(cat) { navController.navigate("cat/${cat.id}") }
+        }
+    }
+}
+
+@Composable
+private fun CatCard(
+    cat: Cat,
+    onClick: () -> Unit
+) {
+    Row(
+        Modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = onClick)
+            .fillMaxWidth()
+    ) {
+        Surface(
+            modifier = Modifier.size(80.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            Image(painter = painterResource(cat.image), contentDescription = cat.name)
+        }
+        Spacer(Modifier.requiredWidth(24.dp))
+        Text(cat.name, fontSize = 24.sp)
+    }
+}
