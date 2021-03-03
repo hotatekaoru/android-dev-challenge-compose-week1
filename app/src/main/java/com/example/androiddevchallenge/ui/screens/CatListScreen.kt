9640f6a56ffee214e7086ada65ce1375fbc72caa
hotatekaoru <kaorupochi@gmail.com>
2021-03-03 23:36:31 +0900

    Show Cat List preview

--- app/src/main/java/com/example/androiddevchallenge/ui/screens/CatListScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/CatListScreen.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -112,4 +113,13 @@ private fun CatExplain(cat: Cat) {
         Spacer(Modifier.requiredHeight(4.dp))
         Text(cat.hometown, color = Color.Gray, fontSize = 16.sp)
     }
+}
+
+@Composable
+@Preview
+private fun CatListPreview() {
+    CatList(
+        cats = catsSeed,
+        onClick = {}
+    )
 }
